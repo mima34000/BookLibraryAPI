f970ae9362a3f6186e1276d0587c5be10eca9974
Mirjana Ristic <mima34.mg@gmail.com>
2026-05-02 14:17:08 +0200

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,20 @@
+﻿namespace BookLibraryAPI.Models
+{
+    public class Book
+    {
+        // unique id for each book
+        public int Id { get; set; }
+
+        // book title
+        public string Title { get; set; } = string.Empty;
+
+        // who wrote it
+        public string Author { get; set; } = string.Empty;
+
+        // year it was published
+        public int Year { get; set; }
+
+        // true if i have read it, false if not
+        public bool IsRead { get; set; }
+    }
+}
